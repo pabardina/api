157e1a17031d8b867f6460ea4082b978ad1e1ded
Pierre-Alexandre Bardina <pa.bardina@gmail.com>
2017-02-12 19:20:09 +0100

    Add container mgmt

---
+++ container/container.go
@@ -0,0 +1,53 @@
+package container
+
+import (
+	"fmt"
+	"github.com/fsouza/go-dockerclient"
+	"os"
+)
+
+const (
+	endpoint = "tcp://127.0.0.1:2375"
+)
+
+func ManageContainer() {
+	client, err := docker.NewClient(endpoint)
+
+	if err != nil {
+		panic(err)
+	}
+
+	removeOpts := docker.RemoveContainerOptions{ID: "listener", Force: true}
+
+	if err := client.RemoveContainer(removeOpts); err != nil {
+		panic(err)
+	}
+
+	cmd := []string{
+		"--access-token=" + os.Getenv("ACCESS_TOKEN"),
+		"--access-token-secret=" + os.Getenv("ACCESS_TOKEN_SECRET"),
+		"--consumer-key=" + os.Getenv("CONSUMER_KEY"),
+		"--consumer-secret=" + os.Getenv("CONSUMER_SECRET"),
+		"-l=5", "-r=5",
+	}
+
+	config := docker.Config{Cmd: cmd, Image: "pabardina/hirondelle-listener"}
+	host := docker.HostConfig{NetworkMode: "host"}
+	opts := docker.CreateContainerOptions{
+		Name:       "listener",
+		Config:     &config,
+		HostConfig: &host,
+	}
+	container, err := client.CreateContainer(opts)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := client.StartContainer(container.ID, &host); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(container.ID)
+
+}
